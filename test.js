2811bf72706bbe06ad6ed58980b3465825ede7a4
jasonmichaelphillips <jasonmphillips@gmail.com>
2025-02-11 20:16:29 -0600

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const testKey = 'YOUTUBE_API_KEY_JMP';
-console.log("API Key:", testKey); // Keep this
-console.info("API Key (info):", testKey); // Add this
-console.warn("API Key (warn):", testKey); // Add this
-console.error("API Key (error):", testKey); // Add this
-alert("test.js is running!");
