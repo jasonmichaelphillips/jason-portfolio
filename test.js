75898d8a8c905c9a6d90b5e51b8373b2948cf531
jasonmichaelphillips <jasonmphillips@gmail.com>
2025-02-11 19:34:23 -0600

    upda

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 const testKey = 'YOUTUBE_API_KEY_JMP';
-console.log("API Key:", testKey);
-alert("test.js is running!"); // Add this line
+console.log("API Key:", testKey); // Keep this
+console.info("API Key (info):", testKey); // Add this
+console.warn("API Key (warn):", testKey); // Add this
+console.error("API Key (error):", testKey); // Add this
+alert("test.js is running!");
